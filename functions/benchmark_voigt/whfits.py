1e84f0fb0dcb0a6734260ee6d24d2beff1abc9e5
EvBlay13 <evan.blaylock10@icloud.com>
2025-04-30 17:40:00 -0400

    Update whfits.py

--- functions/benchmark_voigt/whfits.py
+++ functions/benchmark_voigt/whfits.py
@@ -8,6 +8,12 @@ import edibles.utils.benchmark_voigt.errors as e
 import edibles.utils.benchmark_voigt.read_data as rd
 from edibles.utils.benchmark_voigt.benchmark_fit import *
 
+
+#this file produces just the welty and hobbs data, similar to the way auto_fit.py does it
+#just need to switch the variables in the calls of read_benchmark_data and get_lamb_flux, as well as f and g according to the wavelengths
+#in the mail.txt file in the parameter_modelling_data folder
+#could automate this variable changing
+
 #get data for sightlines
 resolution, star_name, central_wavelength = rd.read_benchmark_data(5895.9242,5895)
 f = 3.270e-1
@@ -19,15 +25,17 @@ for i in range(len(star_name)):
     b_WH,N_WH,v_rad_WH = rd.read_WH_data(star_name[i])
     #get star data
     wavelengths, normflux = rd.get_lamb_flux(star_name[i],5895)
-    ## import file and read data
 
+    #errors
     errors = e.errors_flux(wavelengths,normflux)
 
 
 
-
+    #calling fit_multi....
     fittedmodelman = fit_multi_voigt_absorptionlines(wavegrid = wavelengths, ydata = normflux, restwave = central_wavelength, f = f,
                                      gamma = g, b = b_WH, N = N_WH, v_rad = v_rad_WH, v_resolution = 0.50, n_step=nstep, std_dev =errors)
+    
+    #this step prints out each individual component used in the fit
     for comp in range(len(b_WH)):
         # print(comp)
         indivfit = fit_multi_voigt_absorptionlines(wavegrid=wavelengths,
@@ -45,12 +53,12 @@ for i in range(len(star_name)):
         plt.plot(wavelengths, indivfit.best_fit, label=comp)
         plt.legend()
         plt.show()
+
     #plot statement
-
-
     plt.plot(wavelengths, normflux, label='data')
     plt.plot(wavelengths,fittedmodelman.best_fit, label = 'fitted model')
-    print(fittedmodelman.redchi)#matches within 3 decimal places of chi squared given in the fit_data txt
+    #the statistical values
+    print(fittedmodelman.redchi)
     print(fittedmodelman.bic)
     print(fittedmodelman.aic)
     plt.legend()
