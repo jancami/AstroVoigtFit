848f1c18aa53430d13d2700995b88a4fc75dd983
EvBlay13 <evan.blaylock10@icloud.com>
2025-05-01 11:13:55 -0400

    Update errors.py

--- functions/benchmark_voigt/errors.py
+++ functions/benchmark_voigt/errors.py
@@ -1,7 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 def errors_flux(waves, fluxes):
+    """Takes the standard deviation of the first 50 normalized flux measurements 
+        to estimate the error in the data set.
+
+        Args:
+            waves(ndarray): The wavelength data for the sightline
+            fluxes(ndarray): The normalized flux for the sightline
+
+        Returns:
+            errors: ndarray
+                Array of the estimated errors from the first 50 data points"""
+    
+    #standard deviation of the first 50 fluxes
     errors = np.std(fluxes[:50])
+    #plot what the first fifty points look like, to see if its a somewhat random distribution
     plt.plot(waves[:50],fluxes[:50])
     return errors
 
