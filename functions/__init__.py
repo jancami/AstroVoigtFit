f9e7cc0e83b9b6952ae012719fe26b15bce434de
EvBlay13 <evan.blaylock10@icloud.com>
2025-05-01 16:34:41 -0400

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,7 +1,16 @@
 import os
 from sys import platform
 
-#this is the only environmental variable so far
+#you will want to create some folders to keep all of your python projects in, i created a folder called home, 
+#then within I created another folder called python, in that folder i store all of my python projects.
+
+
+#set up environmental varibles, I used pycharm so its a little different
+#create environmental variables,you need two: 
+#PYTHONPATH = '/home/python'
+#AVF_PYTHONDIR = '/home/python/AstroVoigtFit
+
+#this is just a check.
 if 'AVF_PYTHONDIR' in os.environ:
     if platform == "win32":
         PYTHONDIR = os.environ['AVF_PYTHONDIR']
